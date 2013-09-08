d4384d7ebba70924700325a68e4f975ec9a67217
rodrigotello <rodrigotello@outlook.com>
2013-09-08 18:58:54 -0500

    Still trying to fix the keys

--- config/initializers/ninsho.rb
+++ config/initializers/ninsho.rb
@@ -8,7 +8,7 @@ Ninsho.setup do |config|
 
 
   #Omniauth Providers
-	if Rails.env == "development" || Rails.env="test"
+	if Rails.env == "development" || Rails.env == "test"
   		config.omniauth :facebook, "1408192656070863", "d6de7f2bfa44c7ca2ce1c29f6e142f39", :scope => 'email'
 	  else
 		config.omniauth :facebook, "156907174517059", "32093ec4ca7161a28c6b9bdb0be36aa1", :scope => 'email'
